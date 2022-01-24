b9047889ee7737f0e0f4e6b52027cdaca6898e00
KevinGuedes <kevinguedes@edu.unifor.br>
2022-01-24 20:20:43 -0300

    feat: project structure

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from "@angular/core";
 
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field'
+
 @NgModule({
-    imports: [],
-    exports: [], //* Exports the material modules imported previously. Make them available outside this module
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule], //* Exports the material modules imported previously to make them available outside this module
 })
 export class MaterialModule { }
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fit-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.sass']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
