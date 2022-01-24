9ed7e15295c84dbcec891b691b6df4b8ffa486b0
KevinGuedes <kevinguedes@edu.unifor.br>
2022-01-24 19:47:20 -0300

    feat: angular material configured

---
+++ src/app/material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    imports: [],
+    exports: [], //* Exports the material modules imported previously. Make them available outside this module
+})
+export class MaterialModule { }
