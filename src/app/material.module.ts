2bdf77f9eee0751101b1e1ca2a40d8f529140516
KevinGuedes <kevinguedes@edu.unifor.br>
2022-01-24 20:58:38 -0300

    feat(signup): display flex example started

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule], //* Exports the material modules imported previously to make them available outside this module
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+    //* Exports the material modules imported previously to make them available outside this module
 })
 export class MaterialModule { }
