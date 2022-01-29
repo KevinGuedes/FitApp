9a6e3f3b9e7a257a4e0249cd5f9ed4aa91ffbb07
KevinGuedes <kevinguedes@edu.unifor.br>
2022-01-28 21:22:08 -0300

    feat(current-training): message for halfway done exercise

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -12,6 +12,7 @@ export class CurrentTrainingComponent implements OnInit {
   @Output() trainingExit: EventEmitter<void> = new EventEmitter<void>();
   public progress: number = 0;
   public timer: any = 0;
+  public keepDefaultMessage: boolean = true;
 
   constructor(private dialog: MatDialog) { }
 
@@ -26,7 +27,10 @@ export class CurrentTrainingComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) this.trainingExit.emit();
-      else this.startOrResumeTimer();
+      else {
+        this.startOrResumeTimer();
+        this.keepDefaultMessage = false;
+      }
     })
   }
 
@@ -37,6 +41,7 @@ export class CurrentTrainingComponent implements OnInit {
   private startOrResumeTimer(): void {
     this.timer = setInterval(() => {
       this.progress += 5;
+      if (this.progress >= 50) this.keepDefaultMessage = false;
       if (this.progress >= 100) clearInterval(this.timer);
     }, 1000);
   }
