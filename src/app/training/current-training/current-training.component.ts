998b431be73ddf3ce7d4f0b05afe5369697d181f
KevinGuedes <kevinguedes@edu.unifor.br>
2022-01-28 21:05:01 -0300

    feat(current-training): stop or resume timer according to dialog result

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training/stop-training.component';
 
@@ -9,6 +9,7 @@ import { StopTrainingComponent } from './stop-training/stop-training.component';
 })
 export class CurrentTrainingComponent implements OnInit {
 
+  @Output() trainingExit: EventEmitter<void> = new EventEmitter<void>();
   public progress: number = 0;
   public timer: any = 0;
 
@@ -16,6 +17,7 @@ export class CurrentTrainingComponent implements OnInit {
 
   public onStop(): void {
     clearInterval(this.timer);
+
     const dialogRef = this.dialog.open(StopTrainingComponent, {
       data: {
         progress: this.progress,
@@ -23,11 +25,16 @@ export class CurrentTrainingComponent implements OnInit {
     })
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) this.trainingExit.emit();
+      else this.startOrResumeTimer();
     })
   }
 
   ngOnInit(): void {
+    this.startOrResumeTimer();
+  }
+
+  private startOrResumeTimer(): void {
     this.timer = setInterval(() => {
       this.progress += 5;
       if (this.progress >= 100) clearInterval(this.timer);
