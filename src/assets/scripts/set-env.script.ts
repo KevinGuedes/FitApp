011cc1e46415aeb2d65e043692803f0023addb61
Kevin Guedes <kevinguedes@edu.unifor.br>
2022-02-02 17:09:32 -0300

    fix(env): location id removed from script

--- src/assets/scripts/set-env.script.ts
+++ src/assets/scripts/set-env.script.ts
@@ -36,7 +36,6 @@ const environmentFileContent = `export const environment = {
         projectId: '${process.env["FIREBASE_PROJECT_ID"]}',
         appId: '${process.env["FIREBASE_APP_ID"]}',
         storageBucket: '${process.env["FIREBASE_STORAGE_BUCKET"]}',
-        locationId: '${process.env["FIREBASE_LOCATION_ID"]}',
         apiKey: '${process.env["FIREBASE_API_KEY"]}',
         authDomain: '${process.env["FIREBASE_AUTH_DOMAIN"]}',
         messagingSenderId: '${process.env["FIREBASE_MESSAGING_SENDER_ID"]}',
